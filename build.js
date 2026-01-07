308c4335e2f97688b0f44072da4be3fc59e2c751
Graziani Leo <leo.graziani@etu.u-bordeaux.fr>
2026-01-07 13:23:28 +0100

    blink + text animation

--- build.js
+++ build.js
@@ -5,12 +5,12 @@ var listWork = document.querySelector('#listWork')
 
 var images = ['odomo.png', 'biosphère.png', 'Installation de Poste.PNG', 'BD.PNG', 'site.png', 'PESTEL.png', 'GenMdp.PNG', 'Quizz.png', 'Memories.png', 'jpo.png', 'graphe4.png', "game.png", "series.png", "livingforest3.png", "birds.png", "chess.png", "anges.png", "blink.png", "lsf.png", "myanylist.png"]
 
-var tabTitre =  ['Odomo', 'Biosphère', 'Installation de poste', 'Création de base de données', "Conception d'un site web", 'Analyse PESTEL', 'Générateur de mot de passe', 'Quizz', 'Memories', 'Jeu Java', 'Exploraiton algo', "Jeu en construction" , "Développement d'appli", "Living Forest", "Birds", "Chess 3D AI", "Anges Mdm", "Blink App", "LSF Recognition", "MyAnyList"]
+var tabTitre =  ['Odomo', 'Biosphère', 'Installation de poste', 'Création de base de données', "Conception d'un site web", 'Analyse PESTEL', 'Générateur de mot de passe', 'Quizz', 'Memories', 'Jeu Java', 'Exploraiton algo', "Jeu 2d rpg" , "Développement d'appli", "Living Forest", "Birds", "Chess 3D AI", "Anges Mdm", "Blink App", "LSF Recognition", "MyAnyList"]
 
 var tabPres = ['Programmation d’une petite station météorologique, avec interface', 'Jeu de plateau avec un ajout des règles succéssives', "Installation d'un poste de travail sous une marchine virtuelle pour un programmeur GO"
 ,"Création d'une base de données ansi que le remplissage", "Conception d'un site web (d'une analyse Pestel d'un entreprise) comportant 3 pages", "Analyse PESTEL d'un entreprise (Delpeyrat)"
 ,"Générateur de mot de passe, avec plusieurs options(nb mdp, nb caractères, type caractères)", "Un quizz avec plusieurs niveau de difficulté ansi qu'un niveau bonus (avec quelques petits secrets)", "Un jeu de Memories en local (à 1 ou deux joueurs) avec plusieurs niveau de difficulté", 
-"Jeu pour appprendre les bases de java qui consiste à déplacer une grenouille jusqu'au nénuphar", "Recherche en profondeur, largeur, algo dijkstra...", "En construction", "Site de catalogue et de suivies de séries "
+"Jeu pour appprendre les bases de java qui consiste à déplacer une grenouille jusqu'au nénuphar", "Recherche en profondeur, largeur, algo dijkstra...", "Jeu 2D rpg affrontant des vagues d'ennemies", "Site de catalogue et de suivies de séries "
 , "Jeu de société coopératif adapté en jeu vidéo en ligne", "Simulation de vol de nuées d'oiseaux (godot)", "Jeu d'échecs 3D avec décor (blender) et IA", "Site des Anges Mdm", "Application (santé) de suivi de clignement des yeux", "Reconnaissance de la Langue des Signes Française (actuellement alphabet)", "Site de catalogue de séries/animés/films ET bien plus"]
 
 var tabPresAnglais = ['Programming a small weather station, with interface', 'Board game with an addition of the rules', "Installation of a workstation under a virtual marchine for a GO programmer"
@@ -20,9 +20,9 @@ var tabPresAnglais = ['Programming a small weather station, with interface', 'Bo
 , "Cooperative board game adapted into a video game", "Simulation of flocks of birds", "Artificial intelligence for chess game", "Site of Angels Mdm", "Eye blink tracking application", "Recognition of French Sign Language"]
 
 var date = ['2022', '2022', '2022', '2022', '2022', '2022', '2023', '2023', '2023', '2023', '2023', "2023", "2024", "2025", "2025", "2025", "2025", "2025", "2025~2026", "2024~2026"]
-var nbLanguage = [1, 1, 1, 1, 1, 1, 2, 2, 2,1,1, 2, 4, 3, 2, 3, 3, 1, 1, 4]
+var nbLanguage = [1, 1, 1, 1, 1, 1, 2, 2, 2,1,1, 2, 4, 3, 2, 3, 3, 2, 2, 4]
 var nbLanguageTotal = nbLanguage.map(x => x).reduce((a, b) => a + b, 0) - 1
-var Language = ['Java', 'Java', 'Shell', 'SQL', 'Html-Css', 'Managment', 'javascript', 'Html-Css', 'javascript', 'Html-Css', 'javascript', 'Html-Css', 'Java', "Java", "Html-Css", "javascript", "PHP", "Html-Css","javascript", "Managment", "javascript", "Html-Css", "React",  "Python", "Godot", "CSharp", "Unity", "Blender", "Html-Css", "javascript", "Nextjs", "Python", "Python", "SQL", "Html-Css", "javascript", "Nextjs"]
+var Language = ['Java', 'Java', 'Shell', 'SQL', 'Html-Css', 'Managment', 'javascript', 'Html-Css', 'javascript', 'Html-Css', 'javascript', 'Html-Css', 'Java', "Java", "Html-Css", "javascript", "PHP", "Html-Css","javascript", "Managment", "javascript", "Html-Css", "React",  "Python", "Godot", "CSharp", "Unity", "Blender", "Html-Css", "javascript", "Nextjs", "Python", "OpenCV","Python", "OpenCV", "SQL", "Html-Css", "javascript", "Nextjs"]
 
 var etude = [true, true, true, true, true, true, false, false, false, true, true, false, true, true, true, false, false, true, false, false]
 var retExHtml = ["Odomo", "Biosphere", "InstallationPoste", "CreationBD", "SiteWebPeste", "PESTEL", "","","","JPO", "JPO","", "Catalogue", "", "birds.mp4", "", "", "", "", ""]
@@ -69,7 +69,7 @@ function build(){
                     var a = document.createElement('a')
                     if (etude[i]){
                         if(retExHtml[i].endsWith('.mp4')) a.href = "retEx/"+retExHtml[i]
-                        else a.href = "retEx/"+retExHtml[i]+".html"
+                        else a.href = retExHtml[i]==""?"": "retEx/"+retExHtml[i]+".html"
                         a.target = "blank"
                     }
                     a.classList.add('linkProjects')
